3d8865970313d9c98fbd16f0f83a89b8028da2a8
Himesh9512 <himeshjangid0000@gmail.com>
2022-12-15 17:45:20 +0530

    Create object on add button pressed

--- script.js
+++ script.js
@@ -1,29 +1,46 @@
-let myBook = []
+let myBook = [];
+let newBook = undefined;
+
+const bookNameInput = document.getElementById('book-name');
+const authorNameInput = document.getElementById('author-name');
+const pagesCountInput = document.getElementById('pages-count');
+const readStatusInput = document.getElementById('read');
+const addBookButton = document.getElementById('add-button');
 
 // Book object constructor
-function Book(title,author,pages,isRead){
+function Book(title, author, pages, isRead) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.isRead = isRead;
 }
 
+
 // Add book card to webpage
-function addBookToLibrary(){
+function addBookToLibrary() {
     // code
 }
 
 // Create object of book
-function createBookObject(){
-    // code
+const createBookObject = function(){
+    let bookName = bookNameInput.value;
+    let authorName = authorNameInput.value;
+    let pagesCount = pagesCountInput.value;
+    let readStatus = readStatusInput.value;
+    
+    newBook = new Book(bookName,authorName,pagesCount,readStatus);
+    
+    console.log(newBook);
 }
 
 // Delete object of book
-function deleteBookObject(){
+function deleteBookObject() {
     // code
 }
 
 // Toggle read status
-function toggleReadStatus(){
+function toggleReadStatus() {
     // code
 }
+
+addBookButton.addEventListener('click', createBookObject);
