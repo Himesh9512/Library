dab1864815fc1002054653ae54fa4c6aa85a0c7a
Himesh9512 <himeshjangid0000@gmail.com>
2022-12-15 17:15:36 +0530

    Create Add-book card

--- script.js
+++ script.js
@@ -2,10 +2,28 @@ let myBook = []
 
 // Book object constructor
 function Book(title,author,pages,isRead){
-    // code
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
 }
 
 // Add book card to webpage
 function addBookToLibrary(){
+    // code
+}
+
+// Create object of book
+function createBookObject(){
+    // code
+}
+
+// Delete object of book
+function deleteBookObject(){
+    // code
+}
+
+// Toggle read status
+function toggleReadStatus(){
     // code
 }
