519acd10fc3aad963e1ded9cf79eee23f4a9b25a
Himesh9512 <himeshjangid0000@gmail.com>
2022-12-15 18:20:51 +0530

    Create new objects using user data

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-let myBook = [];
+let myBooks = [];
 let newBook = undefined;
+const DEFAULT_OBJECTS = [
+    {
+        title: "Atomic Habits",
+        author: "James Clear",
+        pages: "320",
+        isRead: "read"
+    },
+    {
+        title: "The psychology of money",
+        author: "Morgan Housel",
+        pages: "252",
+        isRead: "not read"
+    }
+]
 
 const bookNameInput = document.getElementById('book-name');
 const authorNameInput = document.getElementById('author-name');
@@ -8,39 +22,51 @@ const readStatusInput = document.getElementById('read');
 const addBookButton = document.getElementById('add-button');
 
 // Book object constructor
-function Book(title, author, pages, isRead) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.isRead = isRead;
+class Book {
+    constructor(title, author, pages, isRead) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.isRead = isRead;
+    }
 }
 
-
 // Add book card to webpage
-function addBookToLibrary() {
-    // code
+const addBookToLibrary = (book) => {
+    myBooks.push(book);
+    console.log(myBooks);
 }
 
 // Create object of book
-const createBookObject = function(){
+const createBookObject = () => {
     let bookName = bookNameInput.value;
     let authorName = authorNameInput.value;
     let pagesCount = pagesCountInput.value;
     let readStatus = readStatusInput.value;
-    
-    newBook = new Book(bookName,authorName,pagesCount,readStatus);
-    
+
+    newBook = new Book(bookName, authorName, pagesCount, readStatus);
+
     console.log(newBook);
+    addBookToLibrary(newBook);
 }
 
 // Delete object of book
-function deleteBookObject() {
+const deleteBookObject = () => {
     // code
 }
 
 // Toggle read status
-function toggleReadStatus() {
+const toggleReadStatus = () =>{
     // code
 }
 
+// Push default objects on Page Load
+const addDefaultObject = () => {
+    DEFAULT_OBJECTS.forEach((book)=>{
+        myBooks.push(book);
+    });
+    console.log(myBooks);
+}
 addBookButton.addEventListener('click', createBookObject);
+
+window.onload = addDefaultObject;
